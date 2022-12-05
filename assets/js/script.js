3569a007f401e81ec4d887d798688c55626c3f86
hynookim98 <hynookim98@gmail.com>
2022-12-04 21:55:32 -0800

    finished site without functions

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// !Type ANIMATION no work
+// var typed = new Typed(".typing", {
+//     string: ["First Sentence", "Second Sentence", "Third Sentence"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// })
